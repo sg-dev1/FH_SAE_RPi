bbfe49b108ad78e1180578926f84a935c74dbe50
MaxBauV <max-bauer@eclipso.at>
2022-04-22 13:21:28 +0200

    Initial commit

---
+++ speakerTest.c
@@ -0,0 +1,21 @@
+//#include <stdint.h>
+#include <pigpio.h>
+
+#define SPEAKER_PIN 12
+
+int main() {
+
+    // RPi gpio init
+    if (gpioInitialise()<0) {
+        return 1;
+    }
+
+    gpioSetMode(PIN_LED, PI_OUTPUT);
+
+    while (1) {
+        // Play click
+        gpioWrite(SPEAKER_PIN, 1);
+        sleep(1);
+        gpioWrite(SPEAKER_PIN, 0);
+    }
+}
