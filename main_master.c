70048aa06686a49631293c75c1faaa14c422e962
MaxBauV <max-bauer@eclipso.at>
2022-05-02 10:04:02 +0200

    New branch for master work in progress

--- main_master.c
+++ main_master.c
@@ -76,6 +76,7 @@ int main() {
     printf("took %lu us\n", timestamp); 
 
     // Broadcast timestamp
+    sendto(sockfd, (const char *)msg, strlen(msg), MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
     // Wait X seconds
     // Broadcast "start measurement"
     // Play audio
